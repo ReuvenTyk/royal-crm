ab6223a1a9ca2c64a867f1552b6d333b5757ee38
Reuven Tyk <reuventyk@gmail.com>
2022-04-25 09:01:06 +0300

    fixed for one product at a time

--- products.js
+++ products.js
@@ -1,3 +1,28 @@
+//for One product at a time
+
+let products = [];
+
+function addProducts() {
+  let productName = process.argv.slice(2);
+  if (!productName || productName.length === 0) {
+    console.log("empty");
+    return;
+  }
+
+  products.push({
+    name: productName,
+    id: products.length,
+  });
+
+  products.forEach((product) => {
+    console.log(product);
+  });
+}
+
+addProducts();
+
+/* My solution
+
 let products = [];
 
 function addProducts(product) {
@@ -7,7 +32,10 @@ function addProducts(product) {
     return;
   } else {
     for (let i = 0; i < product.length; i++) {
-      arr.push(product[i]);
+      arr.push({
+        name: product[i],
+        id: arr.length,
+      });
     }
   }
   return arr;
@@ -18,9 +46,10 @@ products = addProducts(process.argv.slice(2));
 //solution 1
 /* for (let i = 0; i < costumers.length; i++) {
   console.log(costumers[i]);
-} */
+} 
 
 //solution 2
 products.forEach((product) => {
   console.log(product);
 });
+ */
