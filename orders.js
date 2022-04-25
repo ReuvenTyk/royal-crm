d7eff2f3b08a90d652b38001700852957904a90f
Reuven Tyk <reuventyk@gmail.com>
2022-04-25 09:03:13 +0300

    fixed orders for one order at a fimt

--- orders.js
+++ orders.js
@@ -1,26 +1,48 @@
+//for One order at a time
+
 let orders = [];
 
-function addProducts(order) {
+function addOrders() {
+  let orderName = process.argv.slice(2);
+  if (!orderName || orderName.length === 0) {
+    console.log("empty");
+    return;
+  }
+
+  orders.push({
+    name: orderName,
+    id: orders.length,
+  });
+
+  orders.forEach((order) => {
+    console.log(order);
+  });
+}
+
+addOrders();
+
+/*My solution multi orders
+ let orders = [];
+
+function addOrders(order) {
   let arr = [];
   if (order == "") {
     console.log("empty");
     return;
   } else {
     for (let i = 0; i < order.length; i++) {
-      arr.push(order[i]);
+      arr.push({
+        name: order[i],
+        id: arr.length,
+      });
     }
   }
   return arr;
 }
 
-products = addProducts(process.argv.slice(2));
+products = addOrders(process.argv.slice(2));
 
-//solution 1
-/* for (let i = 0; i < costumers.length; i++) {
-  console.log(costumers[i]);
-} */
-
-//solution 2
-products.forEach((order) => {
+orders.forEach((order) => {
   console.log(order);
 });
+ */
