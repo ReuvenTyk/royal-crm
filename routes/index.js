12cee6cd0286573f12967bc42c4ecc49f17bf8a4
Reuven Tyk <reuventyk@gmail.com>
2022-05-11 18:04:44 +0300

    from callbacks to call from router

--- routes/index.js
+++ routes/index.js
@@ -6,38 +6,34 @@ const ordersModule = require("../controllers/orders");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  //res.send("another change");
+  res.send("this is the home page. use /customers or /products or /orders");
+});
+router.get("/customers", customersModule.customersList);
+router.get("/products", productsModule.productsList);
+router.get("/orders", ordersModule.ordersList);
+
+//calling the function without callback
+//callback function
+/*router.get("/customers", function (req, res, next) {
+  res.send("another change");
   customersModule.addCustomers(
     "Mr. Green",
     "+1 (021) 145-2256",
     "green@blueberrye.com",
     3
-  );
+    );
+    customersModule.customersList(req, res);
+}); 
 
-  customersModule.addCustomers(
-    "Brun Tech",
-    "+251 966 522456",
-    "info@brun-tech.com",
-    5
-  );
-
-  customersModule.addCustomers(
-    "Bakala - Grocery Store",
-    "+972 3 7070 111",
-    "bakala@gmail.com",
-    1
-  );
-  customersModule.customersList(req, res);
-});
-
-router.get("/products", function (req, res, next) {
+ router.get("/products", function (req, res, next) {
   productsModule.addProducts("Milk", "something to drink", "5.7");
+  productsModule.addProducts("Water", "for the soul", "2.7");
   productsModule.productsList(req, res);
-});
+}); 
 
-router.get("/orders", function (req, res, next) {
-  ordersModule.addOrders("5.7", 2);
+ router.get("/orders", function (req, res, next) {
+  ordersModule.addOrders(62, 1, "5.7", 2);
   ordersModule.ordersList(req, res);
-});
+}); */
 
 module.exports = router;
