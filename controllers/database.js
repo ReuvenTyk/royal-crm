f0e4378c0769fd112581e1d49e93b695d26e6241
Reuven Tyk <reuventyk@gmail.com>
2022-05-11 18:06:56 +0300

    joining the parameters and config to one file

--- controllers/database.js
+++ controllers/database.js
@@ -14,6 +14,27 @@ const pool = mysql.createPool({
   queueLimit: 0,
 });
 
+function getConnection() {
+  return new Promise(function (res, rej) {
+    pool.getConnection(function (connErr, connection) {
+      if (connErr) rej(connErr); //not connected!
+      else res(connection);
+    });
+  });
+}
+
+function runQuery(connection, sql) {
+  return new Promise(function (res, rej) {
+    //connection to DB and check the query
+    connection.query(sql, function (sqlErr, result, fields) {
+      if (sqlErr) rej(sqlErr);
+      else res(result);
+    });
+  });
+}
+
 module.exports = {
   pool,
+  getConnection,
+  runQuery,
 };
