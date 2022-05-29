3118e68752f8c19b3f708b7bbb07e21ba3f29ada
Reuven Tyk <reuventyk@gmail.com>
2022-05-29 18:31:10 +0300

    creating router for products and customers

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const cm = require("../controllers/customers");
-const pm = require("../controllers/products");
-const ordersModule = require("../controllers/orders");
 const path = require("path");
+const ordersModule = require("../controllers/orders");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.send("this is the home page. use /customers or /products or /orders");
 });
-//customers
-router.get("/customers-home", function (req, res, next) {
-  const filePath = path.join(__dirname, "../client", "customers-home.html");
+
+router.get("/chat", function (req, res, next) {
+  const filePath = path.join(__dirname, "../client", "chat.html");
   res.sendFile(filePath);
 });
-router.get("/customers", cm.customersList);
-router.post("/customers", cm.addCustomers);
-
-//todo: delete customer
-//router.delete("/customers",cm.deleteCustomer)
-
-//todo: export all customers
-router.get("/customers/export", cm.exportCustomer);
-
-//todo: sort customers by column
-//router.put("/customers");
-
-//todo: edit/update customer
-//router.patch("/customers",cm.findCustomer)
-
-//todo: view more details of a customer
-//router.get("/customer-details",cm.viewCustomersDetails)
-
-//products
-router.get("/products-home", function (req, res, next) {
-  const filePath = path.join(__dirname, "../client", "products-home.html");
-  res.sendFile(filePath);
-});
-router.get("/products", pm.productsList);
-router.post("/products", pm.addProduct);
-router.get("/products/export", pm.exportProducts);
-router.patch("/products", pm.editProducts);
-router.delete("/products", pm.deleteProduct);
-router.get("/products/search/:id", pm.searchProducts);
 
 //orders
 router.get("/orders", ordersModule.ordersList);
