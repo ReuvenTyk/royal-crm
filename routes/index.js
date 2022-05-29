955148011de9f63c311cafc8f0b7463776fa7db2
Reuven Tyk <reuventyk@gmail.com>
2022-05-29 23:39:49 +0300

    chat update

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ const path = require("path");
 router.get("/", function (req, res, next) {
   res.send("this is the home page. use /customers or /products or /orders");
 });
-/* 
+
 router.get("/chat", function (req, res, next) {
   const filePath = path.join(__dirname, "../client", "chat.html");
   res.sendFile(filePath);
-}); */
+});
 
 module.exports = router;
