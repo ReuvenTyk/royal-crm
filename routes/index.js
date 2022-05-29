88e6d77b82033e8e007e27d90333ab791a19a8bb
Reuven Tyk <reuventyk@gmail.com>
2022-05-29 18:32:58 +0300

    cleaning extra notes

--- routes/index.js
+++ routes/index.js
@@ -17,28 +17,4 @@ router.get("/chat", function (req, res, next) {
 router.get("/orders", ordersModule.ordersList);
 router.get("/orders-add", ordersModule.addOrders);
 
-//calling the function without callback
-//callback function
-/*router.get("/customers", function (req, res, next) {
-  res.send("another change");
-  cm.addCustomers(
-    "Mr. Green",
-    "+1 (021) 145-2256",
-    "green@blueberrye.com",
-    3
-    );
-    cm.customersList(req, res);
-}); 
-
- router.get("/products", function (req, res, next) {
-  productsModule.addProducts("Milk", "something to drink", "5.7");
-  productsModule.addProducts("Water", "for the soul", "2.7");
-  productsModule.productsList(req, res);
-}); 
-
- router.get("/orders", function (req, res, next) {
-  ordersModule.addOrders(62, 1, "5.7", 2);
-  ordersModule.ordersList(req, res);
-}); */
-
 module.exports = router;
