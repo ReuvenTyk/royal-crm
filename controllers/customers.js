995290571a926b12d332e2fdb1a5b8f57238935a
Reuven Tyk <reuventyk@gmail.com>
2022-06-01 19:59:20 +0300

    go to MongoDb

--- controllers/customers.js
+++ controllers/customers.js
@@ -1,139 +1,62 @@
 const joi = require("joi");
-const database = require("./database");
+const mongo = require("./database");
 const fileMgmt = require("../shared/fileMgmt");
 
-//const customers
 module.exports = {
-  //send values
-  addCustomers: async function (req, res, next) {
-    //using joi package for validation
+  addCustomer: async function (req, res, next) {
     const reqBody = req.body;
+
     const schema = joi.object({
       name: joi.string().required().min(2).max(200),
       phone: joi
         .string()
         .required()
-        .regex(/^[0-9]\d{8,11}$/),
+        .regex(/^[0-9]{8,11}$/),
       email: joi
         .string()
         .required()
         .regex(/^[^@]+@[^@]+$/),
-      country_id: joi.number().required(),
+      countryId: joi.string().required(),
     });
 
-    const { error } = schema.validate(reqBody);
+    const { error, value } = schema.validate(reqBody);
 
     if (error) {
       res.send(`error adding customer: ${error}`);
       return;
     }
 
-    const sql =
-      "INSERT INTO customers(name,phone,email,country_id)" + "VALUES(?,?,?,?)";
     try {
-      const result = await database.query(sql, [
-        reqBody.name,
-        reqBody.phone,
-        reqBody.email,
-        reqBody.country_id,
-      ]); //getting back an array [row,fields]
+      const database = await mongo.getDb();
+      const collection = database.collection("customers");
+      collection.insertOne(value); // { name: '', phone..., email}
+      res.json(value);
     } catch (err) {
       console.log(err);
-      return;
+      res.status(400).send(`error adding customer`);
     }
-    res.send(`${reqBody.name} added successfully`);
   },
 
   customersList: async function (req, res, next) {
     const param = req.query; // get method
-    //const param = req.body;// post method
-
-    const schema = joi.object({
-      column: joi.string().valid("name", "email", "country_id").default("name"),
-      sort: joi.string().valid("ASC", "DESC").default("ASC"),
-    });
-
-    const { error, value } = schema.validate(param);
-
-    /* 
-      mapping:
-      if the key then give me the value
-
-      example:
-      fieldsMap.get('email'); =>'customer.email
-    */
-
-    const fieldMap = new Map([
-      // key , value
-      ["name", "customers.name"],
-      ["email", "customers.email"],
-      ["country_id", "customers.country_id"],
-    ]);
-
-    //get the DB
-    const sql = `SELECT customers.name AS name, customers.phone AS phone, customers.email AS email, countries.name AS country_name, countries.country_code AS country_code FROM customers JOIN countries ON customers.country_id = countries.id 
-    ORDER BY ${fieldMap.get(value.column)} ${value.sort}`;
-
+    //  const param = req.body;  // post method
     try {
-      //going to mySql2 promise func
-      const result = await database.query(sql); //getting back an array
-      res.send(result[0]);
+      const database = await mongo.getDb();
+      const collection = database.collection("customers");
+
+      const result = await collection
+        .find({})
+        .sort({ name: 1 }) // ASC
+        .toArray();
+
+      res.json(result);
     } catch (err) {
       console.log(err);
-      res.send(err);
+      res.status(400).send(err);
     }
   },
 
-  //export all customers to file
-  exportCustomer: function (req, res, next) {
-    const sql =
-      "SELECT customers.name, customers.phone, customers.email, countries.name AS country_name, countries.country_code AS country_code FROM customers customers JOIN countries ON customers.country_id = countries.id ORDER BY customers.name ASC";
-
-    fileMgmt.exportToFie(res, sql, "customers");
+  exportCustomers: function (req, res, next) {
+    fileMgmt.exportToFile(res, "customers");
   },
-  // search in customers by parameters
-  findCustomer: async function (req, res, next) {
-    const param = req.query;
-
-    const schema = joi.object({
-      //the name that we'll right in the name at the HTML
-      search: joi.string().required().min(2),
-    });
-
-    const { error, value } = schema.validate(param);
-
-    if (error) {
-      res.status(400).send(`Search in Invalid: ${error}`);
-      throw error;
-    }
-
-    const searchQuery = `%${value.search}%`;
-
-    const sql = `SELECT customers.name AS name, customers.phone AS phone, customers.email AS email, countries.name AS country_name, countries.country_code AS country_code FROM customers JOIN countries ON customers.country_id = countries.id 
-    WHERE customers.name LIKE ? OR customers.email LIKE ? OR country_id LIKE ?
-    ORDER BY customers.name ASC; `;
-
-    try {
-      const result = await database.query(sql, [
-        //checking each ? in the sql above
-        searchQuery,
-        searchQuery,
-        searchQuery,
-      ]);
-      res.send(result[0]);
-    } catch (err) {
-      res.status(400).send(`Search in Invalid: ${error}`);
-      throw error;
-    }
-  },
-
-  //todo: delete customer
-  //sql: DROP
-  deleteCustomer: async function (req, res, next) {},
-
-  //todo: edit/update customer
-  updateCustomer: async function (req, res, next) {},
-
-  //todo: view more details of a customer
-  viewCustomersDetails: async function (req, res, next) {},
 };
--- controllers/database.js
+++ controllers/database.js
@@ -1,28 +1,16 @@
-//the connection stats - from the configuration file
+const { MongoClient } = require("mongodb");
 const config = require("../config/dev");
-//open connection to the DB
-const mysql = require("mysql2");
 
-//connection pool configuration - the data from the config file
-const pool = mysql.createPool({
-  host: config.DB_HOST,
-  user: config.DB_USER,
-  password: config.DB_PASSWORD,
-  database: config.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 5,
-  queueLimit: 0,
-});
+const client = new MongoClient(config.MONGO_HOST); // connection url
+let db = undefined;
 
-//mySql2 code
-async function query(sql, values) {
-  //making the connection
-  const promisePool = pool.promise();
-  //running the query
-  return ([row, fields] = await promisePool.query(sql, values));
+async function getDb() {
+  if (db) return db;
+
+  await client.connect();
+  return client.db(config.MONGO_DB);
 }
 
 module.exports = {
-  //sending the table back as row and fields
-  query,
+  getDb,
 };
